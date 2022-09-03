c04833a9578c369a4af742fac361e7ca53c37197
Dominic Sumandan <93337211+DominicS98@users.noreply.github.com>
2022-09-03 19:21:16 +0300

    Full code

---
+++ app.js
@@ -0,0 +1,75 @@
+console.log("Script working!")
+let scissorImage = "\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2880\u28E0\u28C4\u2840\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u28B0\u285F\u2809\u2809\u2819\u28E6\u2840\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u28B8\u2847\u2800\u2800\u2800\u2838\u28E7\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2808\u28FF\u2844\u2800\u2800\u28A0\u28FF\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2880\u28E4\u2876\u2836\u28B6\u28E4\u28C4\u28C0\u2818\u281B\u2836\u28F4\u283F\u2803\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u28FE\u2847\u2800\u2800\u2800\u2808\u2819\u28BF\u28FF\u28F7\u28F6\u28E4\u28C4\u28C0\u2840\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2818\u28B7\u28C4\u2840\u2800\u2800\u2880\u28F8\u285F\u2809\u2819\u283B\u28FF\u28FF\u28FF\u28F7\u28F6\u28E4\u28C4\u28C0\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2809\u281B\u281B\u281B\u281B\u2809\u2800\u2800\u28B8\u28E6\u28C4\u2849\u281B\u283F\u28FF\u28FF\u28FF\u28FF\u28FF\u28F6\u28E4\u28C0\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u28BF\u28FF\u28FF\u28C6\u2800\u2800\u2819\u283B\u28BF\u28FF\u28FF\u28FF\u28FF\u28F7\u28C4\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2808\u28FF\u28FF\u28FF\u28E6\u2800\u2800\u2800\u2800\u2808\u2819\u283B\u28BF\u28FF\u28FF\u28F7\u2840\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2838\u28FF\u28FF\u28FF\u28E7\u2840\u2800\u2800\u2800\u2800\u2800\u2800\u2809\u281B\u283F\u28F7\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2839\u28FF\u28FF\u28FF\u28F7\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2819\u28FF\u28FF\u28FF\u28E7\u2840\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2808\u283B\u28FF\u28FF\u28F7\u2840\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2809\u281B\u2813\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800"
+let rockImage = "\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2880\u28E4\u28E4\u2844\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2880\u28E0\u28F4\u28FE\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28F7\u28F6\u28E6\u28E4\u28C0\u28C0\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2880\u28E4\u28F6\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u281F\u2802\u2840\u2800\r\n\u2800\u2800\u2832\u28BF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u285B\u281B\u283B\u283F\u281F\u288B\u28E0\u28F4\u28FF\u2847\u2800\r\n\u2800\u28FE\u28F6\u28E4\u28CC\u2849\u281B\u283B\u28BF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u283F\u28B7\u28E4\u2840\u28B0\u28FF\u28FF\u28FF\u28FF\u2847\u2800\r\n\u2800\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28F6\u28E4\u28E4\u28E4\u28C4\u28C0\u28C8\u28C9\u28C9\u28C9\u2809\u28A0\u28F4\u28FE\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u2847\u2800\r\n\u2800\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u2800\u28B8\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u285F\u2800\u2800\r\n\u2800\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u2800\u28B8\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u2846\u2800\r\n\u2800\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u283F\u283F\u28FF\u28FF\u28FF\u28FF\u28FF\u2800\u28B8\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u2847\u2800\r\n\u2800\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28EF\u28ED\u28E4\u28E4\u28C4\u2840\u2809\u281B\u283F\u28FF\u2800\u28B8\u287F\u28BF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u2847\u2800\r\n\u2800\u2839\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28F7\u28E6\u28C0\u2800\u2800\u28C0\u28F4\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u283F\u280B\u2801\u2800\r\n\u2800\u2800\u2819\u281B\u283F\u283F\u28FF\u28FF\u281F\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u28FF\u283E\u28BF\u28FF\u28FF\u28FF\u28FF\u281F\u280B\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2808\u2819\u281B\u283B\u283F\u28BF\u28FF\u28FF\u2800\u28B8\u283F\u281B\u2809\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\r\n\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2808\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800\u2800"
+let paperImage = "      _.--._  _.--._\r\n,-=.-\":;:;:;\\\':;:;:;\"-._\r\n\\\\\\:;:;:;:;:;\\:;:;:;:;:;\\\r\n \\\\\\:;:;:;:;:;\\:;:;:;:;:;\\\r\n  \\\\\\:;:;:;:;:;\\:;:;:;:;:;\\\r\n   \\\\\\:;:;:;:;:;\\:;::;:;:;:\\\r\n    \\\\\\;:;::;:;:;\\:;:;:;::;:\\\r\n     \\\\\\;;:;:_:--:\\:_:--:_;:;\\   \r\n      \\\\\\_.-\"      :      \"-._\\\r\n       \\`_..--\"\"--.;.--\"\"--.._=>"
+//ascii art of rock paper and scissor
+let playerScore = 0;
+let computerScore = 0;
+let gameRound = 1;
+//deufalt score
+let computerPlay = function () {
+  let randomNumber = Math.floor((Math.random() * 3 + 1));
+  if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "rock";
+  } else {
+    return "scissors";
+  }
+}
+let computerSelection = computerPlay();
+//computer picking randomly paper rock or scissors
+let playRound = function (playerSelection, computerSelection) {
+  if (playerSelection === "paper" && computerSelection === "paper") {
+    console.log(`Round ${gameRound}\nUser picked:\n${paperImage}\n and the computer got: \n${paperImage}\n It's a tie!`);
+    gameRound++;
+  } if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log(`Round ${gameRound}\nUser picked:\n${paperImage}\n and the computer got: \n${rockImage}\n Player Won!`);
+    gameRound++;
+    playerScore++;
+  } if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log(`Round ${gameRound}\nUser picked:\n${paperImage}\n and the computer got: \n${scissorImage}\n Computer Won!`);
+    gameRound++;
+    computerScore++;
+  } if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log(`Round ${gameRound}\nUser picked:\n${rockImage}\n and the computer got: \n${paperImage}\n Computer Won!`);
+    gameRound++;
+    computerScore++;
+  } if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log(`Round ${gameRound}\nUser picked:\n${rockImage}\n and the computer got: \n${scissorImage}\n Player Won!`);
+    gameRound++;
+    playerScore++;
+  } if (playerSelection === "rock" && computerSelection === "rock") {
+    console.log(`Round ${gameRound}\nUser picked:\n${rockImage}\n and the computer got: \n${rockImage}\n It's a tie!`);
+    gameRound++;
+  } if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log(`Round ${gameRound}\nUser picked:\n${scissorImage}\n and the computer got: \n${paperImage}\n Player Won!`);
+    gameRound++;
+    playerScore++;
+  } if (playerSelection === "scissors " && computerSelection === "scissors") {
+    console.log(`Round ${gameRound}\nUser picked:\n${scissorImage}\n and the computer got: \n${scissorImage}\n It's a tie!`);
+    gameRound++;
+  } if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log(`Round ${gameRound}\nUser picked:\n${scissorImage}\n and the computer got: \n${rockImage}\n Computer Won!`);
+    gameRound++;
+    computerScore++;
+  }
+}
+//game runs
+
+let game = function () {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("What do you pick, Paper,Rock or Scissors?").toLowerCase();
+    //player picks
+    console.log(playRound(playerSelection, computerSelection));
+    //game auto plays
+  }
+  if (computerScore > playerScore) {
+    console.log(`You lost! \nPlayer Score:${playerScore} \nComputer Score:${computerScore} `)
+  }
+  if (computerScore < playerScore) {
+    console.log(`You win! \nPlayer Score:${playerScore} \nComputer Score:${computerScore} `)
+  }
+}
+console.log(game());
+//game starts on page load
